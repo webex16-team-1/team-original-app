c1432efeaf36de769795c2e69b063a0167a68b68
Squash-h <h.squash.5103@icloud.com>
2022-12-22 02:57:13 +0900

    投稿?機能

--- src/components/SubmitForm.vue
+++ src/components/SubmitForm.vue
@@ -1,14 +1,33 @@
 <template>
-  <div class="home__wrapper">
-    <div class="form__wrapper">
-      <textarea
-        class="form__textarea"
-        v-model="text"
-        placeholder="いまどうしてる？"
-      />
-      <div class="form__buttons">
-        <button v-on:click="postTweet" class="form__submit-button">投稿</button>
-      </div>
+  <div class="container" id="app" v-cloak>
+    <h1>評価</h1>
+    <div v-if="length">
+      <h2>評価点 : {{ averageScore }}</h2>
+    </div>
+    <form v-on:submit.prevent>
+      <input type="textarea" v-model="comment" />
+      <select v-model="rate">
+        <option value="0">☆☆☆☆☆</option>
+        <option value="1">★☆☆☆☆</option>
+        <option value="2">★★☆☆☆</option>
+        <option value="3">★★★☆☆</option>
+        <option value="4">★★★★☆</option>
+        <option value="5">★★★★★</option>
+      </select>
+      <button v-on:click="submit">送信</button>
+      <button v-on:click="sort">⬇︎</button>
+      <button v-on:click="sortUp">⬆︎</button>
+    </form>
+    <div v-for="(comment, index) in comments" :key="comment.id">
+      <ul>
+        <li>
+          満足度 : <span>{{ "★".repeat(comment.rate) }}</span>
+        </li>
+        <p>
+          コメント : <span>{{ comment.comment }}</span>
+        </p>
+        <button v-on:click="deleteItem(index)">delete</button>
+      </ul>
     </div>
   </div>
 </template>
@@ -17,12 +36,61 @@
 export default {
   data() {
     return {
-      text: "",
+      comment: "",
+      rate: 0,
+      comments: [],
+      sun: 0,
+      length: false, //使いたい変数達
     }
   },
   methods: {
-    postTweet() {
-      alert("投稿機能の完成をお楽しみに！")
+    submit: function () {
+      if (this.comment == "") return
+      if (this.comment.length > 100) return
+      let commentItem = {
+        comment: this.comment,
+        rate: this.rate,
+      }
+      this.comments.unshift(commentItem)
+      this.sun = this.sun + Number(commentItem.rate)
+      this.comment = ""
+      this.rate = 0
+      if (this.comments.length > 0) {
+        this.length = true
+      }
+    },
+    deleteItem: function (index) {
+      this.sun = this.sun - Number(this.comments[index].rate)
+      this.comments.splice(index, 1)
+      if (this.comments.length < 1) {
+        this.length = false
+      }
+    },
+    sortUp: function () {
+      let arr = this.comments
+      arr.sort(function (a, b) {
+        if (a.rate > b.rate) return 1
+        if (a.rate < b.rate) return -1
+        return 0
+      })
+      this.comments = arr
+    },
+    sort: function () {
+      let arr = this.comments
+      arr.sort(function (a, b) {
+        if (a.rate > b.rate) return -1
+        if (a.rate < b.rate) return 1
+        return 0
+      })
+      this.comments = arr
+    },
+  },
+  computed: {
+    averageScore: function () {
+      return (this.sun / this.comments.length).toFixed(2)
+      //算出プロパティと言います。変数averageScoreをhtml側で呼び出すと
+      //自動的に関数内の処理を実行してくれます。他にも監視プロパティというものもありますが監視プロパティに比べて
+      //処理結果をキャッシュしてくれるというのが大きな特徴です。
     },
   },
 }
